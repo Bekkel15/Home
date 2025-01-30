3d11bea6c022b722c661dfc501177cf454fa8b20
bekkel15 <rexup15@gmail.com>
2025-01-30 10:52:18 +0200

    hw-01

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
 function getElementWidth(content, padding, border) {
-    const c = parseFloat(content)
-    const p = parseFloat(padding)
-    const b = parseFloat(border)
-    const borderbox = c + p * 2  + b * 2;
-    return (borderbox)
+    
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding) * 2; 
+    const borderWidth = parseFloat(border) * 2; 
+
+    
+    return contentWidth + paddingWidth + borderWidth;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
